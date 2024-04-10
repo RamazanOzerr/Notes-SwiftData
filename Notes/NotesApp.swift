0465351d7437dcbbf7948e8636695023ae19f7d2
Ramazan Ozer <ozerramazan991@gmail.com>
2024-04-10 11:12:45 +0300

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Ramazan Ozer on 10.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
